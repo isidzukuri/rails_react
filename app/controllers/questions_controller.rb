c1c5cf60e81dfe752255bec20db383d4035fadbb
isidzukuri <axesigon@gmail.com>
2016-10-18 15:22:36 +0300

    clean

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,12 +14,6 @@ class QuestionsController < ApplicationController
   def create
     @item = Question.new(permited_params)
     @item.save
-
-    # if request.xhr?
-    #   render :json => Comment.last(5)
-    # else
-    #   redirect_to comments_path
-    # end
   end
 
   private
@@ -27,50 +21,4 @@ class QuestionsController < ApplicationController
   def permited_params
     params.require(:question).permit(:title, :content)
   end
-
-  # def new
-  #   @item = Question.new
-  # end
-
-  # def create
-  #   authorize! :create, Product
-  #   @item = Product.new(permited_params)
-  #   if @item.save
-  #     add_flash 'Saved successfully'
-  #     redirect_to @item
-  #   else
-  #     render :new
-  #   end
-  # end
-
-  # def update
-  #   @item = Product.find(params[:id])
-  #   if @item.update(permited_params)
-  #     add_flash 'Saved successfully'
-  #     redirect_to @item
-  #   else
-  #     render :edit
-  #   end
-  # end
-
-  # def destroy
-  #   @item = Product.find(params[:id])
-  #   if @item
-  #     @item.destroy
-  #     add_flash 'Destroyed successfully'
-  #   else
-  #     add_flash 'Product not found', :error
-  #   end
-  #   redirect_to root_path
-  # end
-
-  # private
-
-  # def find_item
-  #   @item = Product.eager_load(:category).find(params[:id])
-  # end
-
-  # def permited_params
-  #   params.require(:product).permit(:title, :description, :price, :category_id)
-  # end
 end
