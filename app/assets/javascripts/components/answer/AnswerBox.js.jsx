97821522a624050b035fac5b6e06654d38d5f427
isidzukuri <axesigon@gmail.com>
2016-10-21 17:44:23 +0300

    clean

--- app/assets/javascripts/components/answer/AnswerBox.js.jsx
+++ app/assets/javascripts/components/answer/AnswerBox.js.jsx
@@ -8,8 +8,6 @@ var AnswerBox = React.createClass({
   },
 
   addItem: function(data){
-
-    console.log(data)
     answers = this.state.item.answers.concat([data]);
     this.updateState('answers', answers);
   },
