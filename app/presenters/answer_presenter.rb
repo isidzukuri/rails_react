feedd7a9683df70c1d623ca72542edacf4014a53
isidzukuri <axesigon@gmail.com>
2016-10-22 14:37:03 +0300

    clean

--- app/presenters/answer_presenter.rb
+++ app/presenters/answer_presenter.rb
@@ -1,7 +1,6 @@
 class AnswerPresenter
   def self.full(item, user_id = nil)
     struct = item.as_json(include: :comments)
-    # item['editable'] = user_id == item['user']['id']
     struct['editable'] = user_id == item.user_id
     struct['votes_total'] = item.votes_total
     struct
