03a4f410378941066e840d560584b26a8d937abd
isidzukuri <axesigon@gmail.com>
2016-10-26 13:03:34 +0300

    user page link

--- app/assets/javascripts/components/user/Created.js.jsx
+++ app/assets/javascripts/components/user/Created.js.jsx
@@ -1,10 +1,18 @@
 var Created = React.createClass({
+  redirect: function(event){
+    event.preventDefault();
+    window.location = '/users/' + this.props.item.user.id;
+  },
   render: function () {
     var item = this.props.item;
+    
     return(
       <small className='pull-right text-lowercase'>
-        <img src={item.user.gravatar} /> 
-        { item.user.email } at { item.date }
+        <span className='text-primary' onClick={ this.redirect }>
+          <img src={item.user.gravatar} />&nbsp;
+          { item.user.email }&nbsp; 
+        </span>
+        at { item.date }
       </small>
     ) 
     
